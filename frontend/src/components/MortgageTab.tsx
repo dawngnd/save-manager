1deca2a11c0cb40ce26a9892e6d98996d60eb128
dangnd1 <dangnd1@vnpay.vn>
2026-08-12 15:13:23 +0700

    feat(12-01): create MortgageTab wrapper component

---
+++ frontend/src/components/MortgageTab.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MortgageForm } from './MortgageForm';
+
+export const MortgageTab: React.FC = () => {
+  return (
+    <div className="space-y-5">
+      <MortgageForm />
+    </div>
+  );
+};
